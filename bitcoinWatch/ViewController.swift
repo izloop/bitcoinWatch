415dcc73d973a3fe4fed2f987cb80d9f6d06a611
izloop <peter@ptip.uk>
2019-07-08 01:13:47 +0200

    Initial Commit

---
+++ bitcoinWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitcoinWatch
+//
+//  Created by Izloop on 7/8/19.
+//  Copyright © 2019 Peter Levi Hornig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
